3328a53205d0e6561d1cd56d83700bafdceae286
kenta0202 <naiken0202@gmail.com>
2022-04-15 14:49:53 +0900

    Action Createtorを導入

--- src/pages/Event/EventForm/hooks.ts
+++ src/pages/Event/EventForm/hooks.ts
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import EventPageContext from "../hooks/contexts/EventPageContext";
 import { uid as UID } from "uid";
+import { create, delete_all } from "../hooks/actions";
 
 export const useEventForm = (
   title: string,
@@ -12,7 +13,8 @@ export const useEventForm = (
 
   const addEvent = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
-    dispatch({ type: "event/create", payload: { title, body, uid: UID(8) } });
+    // dispatch({ type: "event/create", payload: { title, body, uid: UID(8) } });
+    dispatch(create(title, body, UID(8)));
     setTitle("");
     setBody("");
   };
@@ -22,7 +24,7 @@ export const useEventForm = (
     const result = window.confirm(
       "すべてのイベントを本当に削除しても良いですか？"
     );
-    result && dispatch({ type: "event/delete_all" });
+    result && dispatch(delete_all());
   };
 
   return {
--- src/pages/Event/EventItem/hooks.ts
+++ src/pages/Event/EventItem/hooks.ts
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import { TEventItem } from "../../../interface/event";
+import { delete_one } from "../hooks/actions";
 import EventPageContext from "../hooks/contexts/EventPageContext";
 
 export const useEvent = (id: TEventItem["id"], uid: TEventItem["uid"]) => {
@@ -8,7 +9,7 @@ export const useEvent = (id: TEventItem["id"], uid: TEventItem["uid"]) => {
     const result = window.confirm(
       `イベント(ID=${id})を本当に削除しても良いですか？`
     );
-    result && dispatch({ type: "event/delete", payload: { uid: uid } });
+    result && dispatch(delete_one(uid));
   };
 
   return { handleOnClickDeleteButton };
